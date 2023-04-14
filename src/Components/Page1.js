9b06186bb3441808e8faed821d7153dadf282005
chandnisingh-design <130735095+chandnisingh-design@users.noreply.github.com>
2023-04-14 17:56:20 +0530

    Update Page1.js

--- src/Components/Page1.js
+++ src/Components/Page1.js
@@ -2,7 +2,7 @@ import React from "react"
 import * as ReactDOM from "react-dom"
 class Hello extends React.Component {
     render() {
-      return <div>Hello C, {this.props.name}!</div>;
+      return <div>Hello Cvdf, {this.props.name}!</div>;
     }
   }
   export default Hello;
