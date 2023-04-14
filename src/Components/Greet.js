bb652283dafc3a213cf4855747252d3ae45b8d2e
chandnisingh-design <130735095+chandnisingh-design@users.noreply.github.com>
2023-04-14 17:23:44 +0530

    Update Greet.js

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,29 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import * as ReactDOM from "react-dom/client"
-
-// function Greet(){
-//     return <h1>Hello Shivangi </h1>
-// }
-
-//Arrow function syntax
-// export const Greet = () => <h1>Hello Amit</h1>
-// const Greet = props => {
-//   // console.log("Hello ",props);
-// //    
-//   return (
-//     <div>
-//       <h1>
-//         Hello...... {props.name}!
-//       </h1>
-//     </div>
-//   );
-// };
-
-
-
 const Greeting = ({ name }) => {
-  return <h1>Hello, {name}</h1>
+  return <h1>HelloG, {name}</h1>
 }
 
 Greeting.propTypes = {
